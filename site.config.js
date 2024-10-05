01466dfb886d7e4796ffc75e882209171c5f2765
Diodalos <diodalos@gmail.com>
2024-10-05 18:11:06 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "diodalos",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/unnamed.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "student...",
     bio: "I develop everything using node.",
     email: "diodalos@gmail.com",
